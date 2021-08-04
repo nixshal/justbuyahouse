e2a34b1ed87be7bc458fce5865e8da5658d6bb4d
nixshal <nixshal@gmail.com>
2021-08-04 19:48:08 +0200

    full scraping from .csv

--- scraper.py
+++ scraper.py
@@ -1,48 +1,126 @@
 import requests
 from bs4 import BeautifulSoup
 from time import sleep
-from itertools import chain
+import itertools
 import pandas as pd
+import numpy as np
 import re
 
+
+d = pd.read_csv(
+    'hardcopy-rent-kl-sentral-438-property-links.csv').values.tolist()
+links = list(itertools.chain(*d))
+test_list = links
+
+print('\n')
+print(test_list)
+print('\n')
+
+
 # BeautifulSoup stuff
-headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/92.0.4515.107 Safari/537.36'}
+headers = {
+    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/92.0.4515.107 Safari/537.36'}
 
-page_num = 0
+rent_id = []
+title = []
+property_price = []
+property_summary = []
+property_address = []
+built_up = []
+land_area = []
+details = []
+property_type = []
+land_title = []
+property_title_type = []
+tenure = []
+built_up_size_sq_ft = []
+built_up_price_per_sq_ft = []
+furnishing = []
+occupancy = []
+unit_type = []
+reference = []
+posted_date = []
+property_features = []
 
-links = []
-cleanlinks = []
-for page in range(0, 3):
-    page_num += 1
-    iproperty_url = 'https://www.iproperty.com.my/rent/all-residential/transport/kl-sentral-438/?page=' + \
-        str(page_num)
-    r = requests.get(iproperty_url, headers=headers)
-    print(r)
+for i in range(len(test_list)):
+    property_url = 'https://www.iproperty.com.my' + test_list[i]
+    r = requests.get(property_url, headers=headers)  # print(r)
     sleep(5)
-    print('Now scraping page ' +str(page_num))
     soup = BeautifulSoup(r.content, 'html.parser')
-    for link in soup.findAll('a'):
-        # print(link.get('href'))
-        links.append(link.get('href'))
 
-links = [i for i in links if i] #removes None
-print(len(links))
-print('**********SCRAPING COMPLETE**********')
+    prop_name = soup.find_all(
+        'h1', class_='PropertySummarystyle__ProjectTitleWrapper-kAhflS PNQmp')[0].text
 
-to_remove = ['http','tel','photo','video','floorplan']
+    rent_id.append(property_url.split('/')[6])
+    title.append(soup.find_all('title')[0].text)
+    str_price = soup.find_all('div', class_='ListingPrice__Price-cYBbuG cspQqH property-price')[
+        0].text.split(' ')[2].replace(',', '')
+    property_price.append(
+        int(''.join(itertools.takewhile(str.isdigit, str_price))))
+    property_summary.append(soup.find_all(
+        'h1', class_='PropertySummarystyle__ProjectTitleWrapper-kAhflS PNQmp')[0].text)
+    property_address.append(soup.find_all(
+        'span', class_='property-address rent-default')[0].text)
+    built_up.append(soup.find_all(
+        'li', class_='PropertySummarystyle__AreaInfoItem-NjZCY dUovgc')[0].text.split(': ')[1])
+    land_area.append(soup.find_all(
+        'li', class_='PropertySummarystyle__AreaInfoItem-NjZCY dUovgc')[1].text.split(': ')[1])
+    details.append(str(soup.find('pre')).split('>')[1].splitlines())
+    property_type.append(soup.find_all(
+        'div', class_='PropertyDetailsListstyle__AttributeItemData-jpQfWB HUTFZ')[0].text)
+    land_title.append(soup.find_all(
+        'div', class_='PropertyDetailsListstyle__AttributeItemData-jpQfWB HUTFZ')[1].text)
+    property_title_type.append(soup.find_all(
+        'div', class_='PropertyDetailsListstyle__AttributeItemData-jpQfWB HUTFZ')[2].text)
+    tenure.append(soup.find_all(
+        'div', class_='PropertyDetailsListstyle__AttributeItemData-jpQfWB HUTFZ')[3].text)
+    built_up_size_sq_ft.append(int(''.join(itertools.takewhile(str.isdigit, soup.find_all(
+        'div', class_='PropertyDetailsListstyle__AttributeItemData-jpQfWB HUTFZ')[4].text.split(' ')[0].replace(',', '')))))
+    built_up_price_per_sq_ft.append(float(soup.find_all(
+        'div', class_='PropertyDetailsListstyle__AttributeItemData-jpQfWB HUTFZ')[5].text.split(' ')[1]))
+    furnishing.append(soup.find_all(
+        'div', class_='PropertyDetailsListstyle__AttributeItemData-jpQfWB HUTFZ')[6].text)
+    occupancy.append(soup.find_all(
+        'div', class_='PropertyDetailsListstyle__AttributeItemData-jpQfWB HUTFZ')[7].text)
+    unit_type.append(soup.find_all(
+        'div', class_='PropertyDetailsListstyle__AttributeItemData-jpQfWB HUTFZ')[8].text)
+    reference.append(int(soup.find_all(
+        'div', class_='PropertyDetailsListstyle__AttributeItemData-jpQfWB HUTFZ')[9].text))
+    posted_date.append(pd.to_datetime(soup.find_all(
+        'div', class_='PropertyDetailsListstyle__AttributeItemData-jpQfWB HUTFZ')[10].text))
+    property_features.append([i.text for i in soup.find_all(
+        'div', class_='attribute-title-container')])
 
-filtered_list = [ i for i in links if "http" not in i and "tel" not in i and "photo" not in i and "video" not in i and "floorplan" not in i and len(i) > 10]
+    print('********** ' + str(i) + ' Data scraped for: ' +
+          prop_name + ' **********')
 
-# for loop implementation
-# for i in links:
-#     if "http" not in i and "tel" not in i and "photo" not in i and "video" not in i and "floorplan" not in i and len(i) > 10:
-#         nlist.append(i)
+print('\n********** SCRAPE COMPLETED **********')
 
-df = pd.DataFrame(filtered_list, columns=["links"])
 
-df.drop_duplicates(inplace=True)
+# Adding to a DataFrame
+columns = ['rent_id', 'title', 'property_price', 'property_summary', 'property_address', 'built_up', 'land_area', 'details', 'property_type', 'land_title',
+           'property_title_type', 'tenure', 'built_up_size_sq_ft', 'built_up_price_per_sq_ft', 'furnishing', 'occupancy', 'unit_type', 'reference',
+           'posted_date', 'property_features']
 
-filename = iproperty_url.split('/')
-filename = filename[3] + '-' + filename[6] + '.csv'
+kl_sentral = pd.DataFrame({'rent_id': rent_id,
+                           'title': title,
+                           'property_price': property_price,
+                           'property_summary': property_summary,
+                           'property_address': property_address,
+                           'built_up': built_up,
+                           'land_area': land_area,
+                           'details': details,
+                           'property_type': property_type,
+                           'land_title': land_title,
+                           'property_title_type': property_title_type,
+                           'tenure': tenure,
+                           'built_up_size_sq_ft': built_up_size_sq_ft,
+                           'built_up_price_per_sq_ft': built_up_price_per_sq_ft,
+                           'furnishing': furnishing,
+                           'occupancy': occupancy,
+                           'unit_type': unit_type,
+                           'reference': reference,
+                           'posted_date': posted_date,
+                           'property_features': property_features})[columns]
 
-df.to_csv(filename, index=False) #to print to a csv
+kl_sentral.to_excel('kl_sentral.xlsx')
