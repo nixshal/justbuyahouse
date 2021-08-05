675de8cf0231a43b78c70a211e947878ba719c48
nixshal <nixshal@gmail.com>
2021-08-05 10:06:38 +0200

    working on updating all scrape calls into Task class

--- scraper.py
+++ scraper.py
@@ -6,8 +6,25 @@ import pandas as pd
 import numpy as np
 import re
 
+# To do
+# use classes??? https://stackoverflow.com/a/65408311 DONE
+# ValueError from property_details column cause array not same length DONE
+# fix classes and add all the functions there to scraped
+# how to get the phone number and agent etc? use JSON? https://stackoverflow.com/questions/67515161/beautiful-soup-returns-an-empty-string-when-website-has-text
+
 
 class Task():
+    # START: Standard data on each property page
+    def rent_id(self):
+        if property_url != None:
+            rent_id.append(property_url.split('/')[6])
+            print('Rent ID                    : ' + 'Success')
+        else:
+            print('Rent ID                   : '+'NaN')
+            rent_id.append('NaN')
+    # END: Standard data on each property page
+
+    # START: Data in property details container (can vary)
     def property_type(self):
         if 'Property Type' in details_dict:
             temp = details_dict['Property Type']
@@ -138,16 +155,22 @@ class Task():
             print('Posted Date                      : ' + 'NaN')
             posted_date.append('NaN')
 
-    def get_method(self, method_name):
+    def property_features(self):
+        if 'Posted Date' in details_dict:
+            temp = details_dict['Posted Date']
+            posted_date.append(soup.find_all(
+                'div', class_='PropertyDetailsListstyle__AttributeItemData-jpQfWB HUTFZ')[temp].text)
+            print('Posted Date                      : ' + 'Success')
+        else:
+            print('Posted Date                      : ' + 'NaN')
+            posted_date.append('NaN')
+
+    def get_method(self, method_name):  # method1 = callbyname.get_method(method_name)
         method = getattr(self, method_name)
         return method()
 
-# t = Task()
-# t.property_type()
-# method1 = callbyname.get_method(method_name)
 
-
-print('\nWill begin in 1 seconds')
+print('\n| iProperty.com.my Scraper |')
 sleep(1)
 
 d = pd.read_csv(
@@ -156,22 +179,27 @@ links = list(itertools.chain(*d))
 test_list = links[5:8]
 
 print('\n')
-print(test_list)
+print('List of properties to be scraped...')
+sleep(2)
+text = "{}".format("\n".join(test_list))
+print(text)
 print('\n')
 
 # BeautifulSoup stuff
 headers = {
     'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/92.0.4515.107 Safari/537.36'}
 
-rent_id = []
+# Create empty lists for scraped data to be appended to
+rent_id = []  # FINISHED
+prop_url = []
 title = []
 property_price = []
 property_summary = []
 property_address = []
 built_up = []
 land_area = []
-details = []
-# Property details
+property_details = []
+# START: Property details container
 property_type = []
 land_title = []
 property_title_type = []
@@ -185,11 +213,13 @@ facing_direction = []
 reference = []
 available_date = []
 posted_date = []
+# END: Property details container
 property_features = []
 
 for i in range(len(test_list)):
     property_url = 'https://www.iproperty.com.my' + test_list[i]
-    r = requests.get(property_url, headers=headers)  # print(r)
+    # print(r) #to get website's response
+    r = requests.get(property_url, headers=headers)
     sleep(5)
     soup = BeautifulSoup(r.content, 'html.parser')
 
@@ -199,42 +229,51 @@ for i in range(len(test_list)):
     print('********** ' + str(i) + ' Scraping data for: ' +
           prop_name + ' **********')
 
-    # getting all the data from the website
-    rent_id.append(property_url.split('/')[6])
-    title.append(soup.find_all('title')[0].text)
-    str_price = soup.find_all('div', class_='ListingPrice__Price-cYBbuG cspQqH property-price')[
-        0].text.split(' ')[2].replace(',', '')
-    property_price.append(
-        int(''.join(itertools.takewhile(str.isdigit, str_price))))
-    property_summary.append(soup.find_all(
-        'h1', class_='PropertySummarystyle__ProjectTitleWrapper-kAhflS PNQmp')[0].text)
-    property_address.append(soup.find_all(
-        'span', class_='property-address rent-default')[0].text)
-    built_up.append(soup.find_all(
-        'li', class_='PropertySummarystyle__AreaInfoItem-NjZCY dUovgc')[0].text.split(': ')[1])
-    land_area.append(soup.find_all(
-        'li', class_='PropertySummarystyle__AreaInfoItem-NjZCY dUovgc')[1].text.split(': ')[1])
-    details.append(str(soup.find('pre')).split('>')[1].splitlines())
-
-    # >>> Property details <<< (tricky)
-    # problem here is that proeprty details are different for each property. So need to have conditionals to match the correct list to be updated
+    # Creating a dictionary for property details held in container
+    # Needed because problem here is that proeprty details are different for each property. So need to have conditionals to match the correct list to be updated
     propdetails = soup.find_all(
         'div', class_='PropertyDetailsListstyle__AttributeItemTitle-gtQJBp YzpOn')
     details = [i.text for i in propdetails]
     details = [i.split(':') for i in details]
-    details = [i[0] for i in details]
-    # print(details)
-    val = list(range(0, len(propdetails)))
-    # print(val)
-    details_dict = dict(zip(details, val))
-    # print(details_dict)
-    #['Property Type', 'Land Title', 'Property Title Type', 'Tenure', 'Built-up Size', 'Built-up Price', 'Furnishing', 'Occupancy', 'Facing Direction', 'Reference No.', 'Posted Date']
-    # for example '/property/seputeh/sri-tiara-residences/rent-102350152/'
+    details = [i[0] for i in details]  # print(details)
+    val = list(range(0, len(propdetails)))  # print(val)
+    details_dict = dict(zip(details, val))  # print(details_dict)
 
-    # use classes??? https://stackoverflow.com/a/65408311
+    t = Task()  # create a new Task object that handles the scraping calls
+    t.rent_id()
 
-    t = Task()
+    # getting all the data from the website
 
+    # rent_id.append(property_url.split('/')[6])
+
+    prop_url.append(property_url)
+
+    title.append(soup.find_all('title')[0].text)
+
+    str_price = soup.find_all('div', class_='ListingPrice__Price-cYBbuG cspQqH property-price')[
+        0].text.split(' ')[2].replace(',', '')
+
+    property_price.append(
+        int(''.join(itertools.takewhile(str.isdigit, str_price))))
+
+    property_summary.append(soup.find_all(
+        'h1', class_='PropertySummarystyle__ProjectTitleWrapper-kAhflS PNQmp')[0].text)
+
+    property_address.append(soup.find_all(
+        'span', class_='property-address rent-default')[0].text)
+
+    built_up.append(soup.find_all(
+        'li', class_='PropertySummarystyle__AreaInfoItem-NjZCY dUovgc')[0].text.split(': ')[1])
+
+    land_area.append(soup.find_all(
+        'li', class_='PropertySummarystyle__AreaInfoItem-NjZCY dUovgc')[1].text.split(': ')[1])
+
+    # details.append(str(soup.find('pre')).split('>')[1].splitlines())
+    # dumps everything into one element of a list
+
+    property_details.append(str(soup.find_all('pre')))
+
+    # >>> Property details <<<
     t.property_type()
     t.land_title()
     t.property_title_type()
@@ -248,94 +287,30 @@ for i in range(len(test_list)):
     t.reference()
     t.available_date()
     t.posted_date()
-
-    # property_type.append(soup.find_all(
-    #     'div', class_='PropertyDetailsListstyle__AttributeItemData-jpQfWB HUTFZ')[0].text)
-
-    # land_title.append(soup.find_all(
-    #     'div', class_='PropertyDetailsListstyle__AttributeItemData-jpQfWB HUTFZ')[1].text)
-
-    # property_title_type.append(soup.find_all(
-    #     'div', class_='PropertyDetailsListstyle__AttributeItemData-jpQfWB HUTFZ')[2].text)
-
-    # tenure.append(soup.find_all(
-    #     'div', class_='PropertyDetailsListstyle__AttributeItemData-jpQfWB HUTFZ')[3].text)
-
-    # built_up_size_sq_ft.append(soup.find_all(
-    #     'div', class_='PropertyDetailsListstyle__AttributeItemData-jpQfWB HUTFZ')[4].text.split(' ')[0])
-
-    # built_up_price_per_sq_ft.append(soup.find_all(
-    #     'div', class_='PropertyDetailsListstyle__AttributeItemData-jpQfWB HUTFZ')[5].text.split(' ')[1])
-
-    # furnishing.append(soup.find_all(
-    #     'div', class_='PropertyDetailsListstyle__AttributeItemData-jpQfWB HUTFZ')[6].text)
-
-    # occupancy.append(soup.find_all(
-    #     'div', class_='PropertyDetailsListstyle__AttributeItemData-jpQfWB HUTFZ')[7].text)
-
-    # unit_type.append(soup.find_all(
-    #     'div', class_='PropertyDetailsListstyle__AttributeItemData-jpQfWB HUTFZ')[8].text)
-
-    # facing_direction.append(soup.find_all(
-    #     'div', class_='PropertyDetailsListstyle__AttributeItemData-jpQfWB HUTFZ')[6].text)
-
-    # reference.append(soup.find_all(
-    #     'div', class_='PropertyDetailsListstyle__AttributeItemData-jpQfWB HUTFZ')[9].text)
-
-    # posted_date.append(pd.to_datetime(soup.find_all(
-    #     'div', class_='PropertyDetailsListstyle__AttributeItemData-jpQfWB HUTFZ')[10].text))
-
-    # available.append(pd.to_datetime(soup.find_all(
-    #     'div', class_='PropertyDetailsListstyle__AttributeItemData-jpQfWB HUTFZ')[10].text))
-
     # >>> End of property details <<<
+
     if [i.text for i in soup.find_all('div', class_='attribute-title-container')] != []:
         property_features.append([i.text for i in soup.find_all(
             'div', class_='attribute-title-container')])
     else:
         property_features.append('NaN')
 
-    # print('\n PRINTING VALUES')
-    # print(rent_id)
-    # print(title)
-    # print(property_price)
-    # print(property_summary)
-    # print(property_address)
-    # print(built_up)
-    # print(land_area)
-    # print(details)
-    # print(property_type)
-    # print(land_title)
-    # print(property_title_type)
-    # print(tenure)
-    # print(built_up_size_sq_ft)
-    # print(built_up_price_per_sq_ft)
-    # print(furnishing)
-    # print(occupancy)
-    # print(unit_type)
-    # print(reference)
-    # print(posted_date)
-    # print(property_features)
-
-
 print('\n********** SCRAPE COMPLETED **********')
 
-
 # Adding to a DataFrame
-columns = ['rent_id', 'title', 'property_price', 'property_summary', 'property_address', 'built_up', 'land_area', 'details', 'property_type', 'land_title',
+columns = ['rent_id', 'prop_url', 'title', 'property_price', 'property_summary', 'property_address', 'built_up', 'land_area', 'property_details', 'property_type', 'land_title',
            'property_title_type', 'tenure', 'built_up_size_sq_ft', 'built_up_price_per_sq_ft', 'furnishing', 'occupancy', 'unit_type', 'reference',
            'posted_date', 'property_features', 'facing_direction', 'available_date']
 
-
-#value error from details column
 kl_sentral = pd.DataFrame({'rent_id': rent_id,
+                           'prop_url': prop_url,
                            'title': title,
                            'property_price': property_price,
                            'property_summary': property_summary,
                            'property_address': property_address,
                            'built_up': built_up,
                            'land_area': land_area,
-                           'details': details,
+                           'property_details': property_details,
                            'property_type': property_type,
                            'land_title': land_title,
                            'property_title_type': property_title_type,
